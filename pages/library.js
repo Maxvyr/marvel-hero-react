c517bdfd903301dd6d54fa41861f44cb831f1c7a
maxvyr <maxvid30@gmail.com>
2022-08-02 22:04:51 +0200

    🚧 all component

--- pages/library.js
+++ pages/library.js
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "../styles/Library.module.css";
-import NavLateral from "../component/nav-lateral/nav-lateral";
+import NavLateral from "../component/NavLateral/NavLateral";
 
 export default function Library() {
   return (
     <div className={styles.main}>
       <NavLateral />
       <h1>Library</h1>
+      <input type="text" placeholder="search book" />
+
     </div>
   );
 }
